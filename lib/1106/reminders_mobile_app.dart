66e652176d8f646482adbedeb2879b3674e1489f
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2019-11-06 20:58:21 +0900

    1106 - RemindersMobileApp
    1106 - RemindersMobileApp

---
+++ lib/1106/reminders_mobile_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// https://dribbble.com/shots/7991398-Reminders-Mobile-App/attachments/525586?mode=media
+
+class RemindersMobileApp extends StatefulWidget {
+  @override
+  _RemindersMobileAppState createState() => _RemindersMobileAppState();
+}
+
+class _RemindersMobileAppState extends State<RemindersMobileApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
