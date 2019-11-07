550e7bfba34b17f9852758e3ee22b819bf22f87e
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2019-11-07 12:20:58 +0900

    1107 - instagram app
    1107 - instagram app

---
+++ lib/1107/instagram_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// https://dribbble.com/shots/6151406-Instagram-Dark-or-Light
+
+class InstagramApp extends StatefulWidget {
+  @override
+  _InstagramAppState createState() => _InstagramAppState();
+}
+
+class _InstagramAppState extends State<InstagramApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 import '1106/reminders_mobile_app.dart';
+import '1107/instagram_app.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RemindersMobileApp(),
+//      home: RemindersMobileApp(),  // 11-06
+      home: InstagramApp(),  // 11-07
   );
 }
 }
