9aea1760c80e1be0be5c20d770637593b7fd719e
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2019-11-08 18:17:01 +0900

    appointment app - 1108
    appointment app - 1108

---
+++ lib/1108/appointmentapp.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// https://dribbble.com/shots/8045347-Form-Screen-Accounting-Appointment
+
+class AppointmentApp extends StatefulWidget {
+  @override
+  _AppointmentAppState createState() => _AppointmentAppState();
+}
+
+class _AppointmentAppState extends State<AppointmentApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '1106/reminders_mobile_app.dart';
 import '1107/instagram_app.dart';
+import '1108/appointmentapp.dart';
 import '1108/fitnessapp.dart';
 
 void main() => runApp(MyApp());
@@ -12,7 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
 //      home: RemindersMobileApp(),  // 11-06
 //      home: InstagramApp(),  // 11-07
-      home: FitnessApp(),  // 11-08
+//      home: FitnessApp(),  // 11-08
+      home: AppointmentApp(),  // 11-08
   );
 }
 }
