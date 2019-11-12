67f027b2a4ada9b5e7129cad9d892c5d51880d75
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2019-11-12 15:25:42 +0900

    appbar
    appbar

--- lib/1112/travel_second_page.dart
+++ lib/1112/travel_second_page.dart
@@ -9,6 +9,11 @@ class _TravelSecondPageState extends State<TravelSecondPage> {
   List<Widget> pages;
 
   int _currentIndex = 0;  // bottombar index
+
+  String title = "Discover";
+
+  String _profileImage = "https://cdn.pixabay.com/photo/2019/10/17/14/52/white-4557097__340.jpg";
+
   @override
   Widget build(BuildContext context) {
     _definePages();
@@ -46,7 +51,55 @@ class _TravelSecondPageState extends State<TravelSecondPage> {
 
   _definePages(){
     pages = [
-      Placeholder(color: Colors.redAccent),
+      // first page
+      Padding(
+        padding: const EdgeInsets.only(left: 24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // appbar
+            Container(
+              margin: EdgeInsets.only(right: 24.0),
+              height: 64.0,
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.sort, size: 24.0, color: Colors.black),
+                  Spacer(),
+                  Container(
+                    height: 40.0,
+                    width: 40.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0),
+                      image: DecorationImage(image: NetworkImage(_profileImage), fit: BoxFit.fill)
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // title
+            Text(title,
+            style: TextStyle(color: Colors.black, fontSize: 32.0, fontWeight: FontWeight.bold),
+            ),
+
+            // tabbar
+            Container(
+              height: 64.0,
+              child: Placeholder(),
+            ),
+
+            // tabbar view
+            Flexible(
+              fit: FlexFit.tight,
+              child: Container(
+                child: Placeholder(),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      //
       Placeholder(color: Colors.amber),
       Placeholder(color: Colors.green),
       Placeholder(color: Colors.indigo),
