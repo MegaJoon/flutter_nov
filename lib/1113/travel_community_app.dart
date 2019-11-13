e60f9e79cf2a278d0080a10f1d67f1ce6a6ae148
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2019-11-13 14:13:08 +0900

    traveL_community_app - 1113
    traveL_community_app - 1113

---
+++ lib/1113/travel_community_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// https://dribbble.com/shots/8171324-Travel-Community-App/attachments/569173?mode=media
+
+class TravelCommunityApp extends StatefulWidget {
+  @override
+  _TravelCommunityAppState createState() => _TravelCommunityAppState();
+}
+
+class _TravelCommunityAppState extends State<TravelCommunityApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
