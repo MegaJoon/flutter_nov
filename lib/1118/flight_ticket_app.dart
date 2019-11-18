0df9b4617ff411d7b2161bf64c8d6fdfcfe74ef8
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2019-11-18 13:19:50 +0900

    appbar
    appbar

--- lib/1118/flight_ticket_app.dart
+++ lib/1118/flight_ticket_app.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
 // https://dribbble.com/shots/8243654-Flight-ticket-UI-design-Passenger-details/attachments/604441?mode=media
@@ -10,6 +11,8 @@ class FlightTicketApp extends StatefulWidget {
 class _FlightTicketAppState extends State<FlightTicketApp> {
   Color _color = Color.fromRGBO(1, 100, 212, 1);
   Color _accentColor = Color.fromRGBO(34, 82, 136, 1);
+
+  double padding = 24.0;
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +34,35 @@ class _FlightTicketAppState extends State<FlightTicketApp> {
                 )
               ),
               child: Placeholder(),
+            ),
+          ),
+
+          // appbar
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: SafeArea(
+              top: true,
+              left: true,
+              right: true,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: padding),
+                height: 40.0,
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.arrow_back, size: 28.0, color: Colors.white,),
+                    Spacer(),
+
+                    Badge(
+                      showBadge: true,
+                      badgeColor: Colors.yellow,
+                      position: BadgePosition.topRight(top: 0, right: 0),
+                      child: Icon(Icons.notifications, size: 28.0, color: Colors.white,),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ],
