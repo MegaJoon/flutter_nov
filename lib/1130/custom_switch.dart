6a2d21d46c8af983ebfda8a47bdef3fdf8a3d6f4
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2019-11-30 15:11:53 +0900

    set background container & appbar
    set background container & appbar

--- lib/1130/custom_switch.dart
+++ lib/1130/custom_switch.dart
@@ -8,10 +8,43 @@ class CustomSwitch extends StatefulWidget {
 }
 
 class _CustomSwitchState extends State<CustomSwitch> {
+  // color list
+  Color _backgroundColor = Colors.black;
+
+  double padding = 16.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Placeholder(),
+      body: Stack(
+        children: <Widget>[
+          // background color container
+          Positioned.fill(
+              child: Container(
+                color: _backgroundColor,
+              ),
+          ),
+
+          // appbar
+          Positioned(
+            top: 0,
+            left: 0,
+            child: SafeArea(
+              top: true,
+              left: true,
+              child: IconButton(
+                onPressed: (){
+                  print("on clicked: ---");
+                },
+
+                icon: Icon(Icons.arrow_back_ios, size: 24.0, color: Colors.white),
+              ),
+            ),
+          ),
+
+          
+        ],
+      ),
     );
   }
 }
